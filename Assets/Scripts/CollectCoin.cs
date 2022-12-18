0742453a745ae8f6ef5c0d82937054f06c3602dc
Osman Canboğa <osmancanboga@hotmail.com>
2022-12-18 15:03:06 +0300

    Scene Manager
    Scene Manager

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CollectCoin : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI coinText;
+    public PlayerController playerController;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Coin")) {
+        if (other.CompareTag("Coin"))
+        {
             Destroy(other.gameObject);
             AddCoin();
-            Debug.Log("Coine Çarptı");
+        }
+        else if(other.CompareTag("End"))
+        {
+            playerController.runningSpeed = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Collision")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
