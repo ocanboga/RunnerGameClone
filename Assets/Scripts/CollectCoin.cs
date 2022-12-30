8b0e59753d531071665be613951b0a91305c0da1
Osman Canboğa <osmancanboga@hotmail.com>
2022-12-30 20:41:40 +0300

    FinalUpdate

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -7,10 +7,18 @@ using UnityEngine.SceneManagement;
 public class CollectCoin : MonoBehaviour
 {
     public int score;
+    public int maxScore;
     public TextMeshProUGUI coinText;
     public PlayerController playerController;
+    public Animator PlayerAnim;
+    public GameObject EndPanel;
+    private void Start()
+    {
+        PlayerAnim = this.GetComponentInChildren<Animator>();
+    }
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Coin"))
         {
             Destroy(other.gameObject);
@@ -18,8 +26,20 @@ public class CollectCoin : MonoBehaviour
         }
         else if(other.CompareTag("End"))
         {
+            EndPanel.SetActive(true);
             playerController.runningSpeed = 0;
+            transform.Rotate(transform.rotation.x, 180, transform.rotation.z, Space.Self);
+            if(score >= maxScore)
+            {
+                PlayerAnim.SetBool("Win",true);
+            }
+            else PlayerAnim.SetBool("Lose", true);
+
         }
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnCollisionEnter(Collision collision)
